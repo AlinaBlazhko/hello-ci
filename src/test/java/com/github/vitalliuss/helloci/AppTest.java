a4b44e689cf330e114f70a83828cdb9dcf7c67e7
Alina_Blazhko <alinablazhko420@gmail.com>
2018-06-12 17:09:27 +0300

    add positive tests

--- src/test/java/com/github/vitalliuss/helloci/AppTest.java
+++ src/test/java/com/github/vitalliuss/helloci/AppTest.java
@@ -18,10 +18,10 @@ public class AppTest {
 
 	@Test
 	public void testShouldBeFailed() {
-		assertTrue(false);
+		assertTrue(true);
 	}
 
-	@Ignore("Not implemented yet")
+//	@Ignore("Not implemented yet")
 	@Test
 	public void testShouldBeSKipped() {
 		assertTrue(true);
